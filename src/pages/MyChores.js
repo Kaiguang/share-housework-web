184541ee6900588e49578e9192d158d6fdfbe4b6
Kaiguang Li <33639861+Kaiguang@users.noreply.github.com>
2020-10-01 15:27:05 -0600

    Add feature to delete chore

--- src/pages/MyChores.js
+++ src/pages/MyChores.js
@@ -6,6 +6,9 @@ import { makeStyles } from "@material-ui/core/styles";
 import Box from "@material-ui/core/Box";
 import Button from "@material-ui/core/Button";
 import CircularProgress from "@material-ui/core/CircularProgress";
+import Dialog from "@material-ui/core/Dialog";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogTitle from "@material-ui/core/DialogTitle";
 import Fab from "@material-ui/core/Fab";
 import List from "@material-ui/core/List";
 import Paper from "@material-ui/core/Paper";
@@ -78,7 +81,13 @@ export default function MyChores() {
           <CircularProgress />
         </Box>
       ) : (
-        myChores.map((item) => <Chore key={item.TimeCreated} chore={item} />)
+        myChores.map((item) => (
+          <Chore
+            key={item.TimeCreated}
+            chore={item}
+            fetchMyChores={fetchMyChores}
+          />
+        ))
       )}
       <Fab
         onClick={handleNewChoreIconClick}
@@ -100,6 +109,8 @@ function Chore(props) {
   const classes = useStyles();
   const [chore, setChore] = useState(props.chore);
   const [isConfirmPaymentLoading, setIsConfirmPaymentLoading] = useState(false);
+  const [isEditDialogOpen, setIsEditDialogOpen] = useState(false);
+  const [isDeleting, setIsDeleting] = useState(false);
 
   useEffect(() => {
     setChore(props.chore);
@@ -125,49 +136,97 @@ function Chore(props) {
       });
   };
 
+  const handleEditButtonClick = () => {
+    setIsEditDialogOpen(true);
+  };
+
+  const deleteChore = (timeCreated) => {
+    setIsDeleting(true);
+
+    API.del(config.Amplify.API.endpoints[0].name, "/deleteChore", {
+      body: { timeCreated },
+    })
+      .then(() => {
+        setIsEditDialogOpen(false);
+        props.fetchMyChores();
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
-    <Paper className={classes.paper}>
-      <Box className={classes.choreRow}>
-        <Typography component="span">{`${chore.Chore} `}</Typography>
-        <Typography variant="caption" color="textSecondary">
-          {new Date(Number(chore.TimeCreated)).toDateString()}
-        </Typography>
-      </Box>
+    <>
+      <Paper className={classes.paper}>
+        <Box className={classes.choreRow}>
+          <Typography component="span">{`${chore.Chore} `}</Typography>
+          <Typography variant="caption" color="textSecondary">
+            {new Date(Number(chore.TimeCreated)).toDateString()}
+          </Typography>
+        </Box>
 
-      <Box className={classes.choreRow}>
-        <Typography component="span" className="cents">
-          ${chore.Cents / 100}
-        </Typography>
-        <Typography
-          className="is-confirmed"
-          color={chore.IsConfirmed ? "primary" : "textSecondary"}
-        >
-          {chore.IsConfirmed
-            ? chore.IsPaid
-              ? "Paid"
-              : "Not Paid"
-            : "Not Confirmed"}
-        </Typography>
-
-        {/*
-        {!chore.IsPaid && !chore.IsConfirmed ? (
-          <Button variant="outlined" size="small">
-            Edit
-          </Button>
-        ) : null}
-        */}
-
-        {!chore.IsPaid && chore.IsConfirmed ? (
-          <Button
-            variant="outlined"
-            size="small"
-            onClick={() => handleConfirmPayment(chore.TimeCreated)}
-            disabled={isConfirmPaymentLoading}
+        <Box className={classes.choreRow}>
+          <Typography component="span" className="cents">
+            ${chore.Cents / 100}
+          </Typography>
+          <Typography
+            className="is-confirmed"
+            color={chore.IsConfirmed ? "primary" : "textSecondary"}
           >
-            {isConfirmPaymentLoading ? `Loading...` : `Confirm Payment`}
-          </Button>
-        ) : null}
-      </Box>
-    </Paper>
+            {chore.IsConfirmed
+              ? chore.IsPaid
+                ? "Paid"
+                : "Not Paid"
+              : "Not Confirmed"}
+          </Typography>
+
+          {!chore.IsPaid && !chore.IsConfirmed ? (
+            <Button
+              variant="outlined"
+              size="small"
+              onClick={() => handleEditButtonClick(chore.TimeCreated)}
+            >
+              Edit
+            </Button>
+          ) : null}
+
+          {!chore.IsPaid && chore.IsConfirmed ? (
+            <Button
+              variant="outlined"
+              size="small"
+              onClick={() => handleConfirmPayment(chore.TimeCreated)}
+              disabled={isConfirmPaymentLoading}
+            >
+              {isConfirmPaymentLoading ? `Loading...` : `Confirm Payment`}
+            </Button>
+          ) : null}
+        </Box>
+      </Paper>
+      <Dialog
+        open={isEditDialogOpen}
+        onClose={() => setIsEditDialogOpen(false)}
+      >
+        <DialogTitle>{isDeleting ? `Deleting...` : `Edit`}</DialogTitle>
+        {isDeleting ? null : (
+          <DialogContent>
+            <Typography>Chore: {chore.Chore}</Typography>
+            <Typography>
+              Date: {new Date(Number(chore.TimeCreated)).toDateString()}
+            </Typography>
+            <Typography>Price: ${chore.Cents / 100}</Typography>
+            <Button
+              variant="outlined"
+              fullWidth
+              color="secondary"
+              onClick={() => {
+                deleteChore(chore.TimeCreated);
+              }}
+            >
+              Delete
+            </Button>
+          </DialogContent>
+        )}
+      </Dialog>
+    </>
   );
 }
